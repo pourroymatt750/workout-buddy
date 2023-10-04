e45df4278de5208934223fa392d7e75e9988acfb
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-04 12:58:06 -0400

    env issue fixed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // Express app
@@ -9,6 +11,7 @@ app.get('/', (req, res) => {
 })
 
 // Listen for requests
-app.listen(4000, () => {
-  console.log('Listening on port 4000')
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port ', process.env.PORT)
 })
+
