de970311d6966f6eba98807e84bcd1a71361ebac
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-04 11:51:28 -0400

    add first route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // Express app
 const app = express()
 
+// Routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+
 // Listen for requests
 app.listen(4000, () => {
   console.log('Listening on port 4000')
