3977525a095453fccdbb0c0faebb486037d56322
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-04 20:32:20 -0400

    add createWorkout controlller

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,1 +1,27 @@
 const Workout = require('../models/workoutModel')
+
+// get all workouts
+
+
+// get a single workout
+
+
+// create a new workout
+const createWorkout = async (req, res) => {
+  const {title, load, reps} = req.body
+
+  // add document to DB
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// update a workout
+
+
+module.exports = {
+  createWorkout
+}
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const {
+  createWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
@@ -13,16 +16,7 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const {title, load, reps} = req.body
-
-  try {
-    const workout = await Workout.create({title, load, reps})
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
