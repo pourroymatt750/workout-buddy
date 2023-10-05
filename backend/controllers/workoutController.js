c720470456c096eb7855115156bf5ea5fa851bf3
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-04 21:22:47 -0400

    add getWorkouts function

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,7 +1,11 @@
 const Workout = require('../models/workoutModel')
 
 // get all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 })
 
+  res.status(200).json(workouts)
+} 
 
 // get a single workout
 
