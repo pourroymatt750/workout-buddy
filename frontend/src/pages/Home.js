c98d60ceb70ae5f1554847491221d9696f69f7cf
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-05 15:47:16 -0400

    connect front and backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const response = await fetch('/api/workouts')
+        if (!response.ok) {
+          throw new Error('Network response was not okay')
+        }
+        const json = await response.json()
+        setWorkouts(json)
+      } catch (error) {
+        console.log('Error fetching workouts: ', error)
+      }
+    }
+    fetchWorkouts()
+  }, [])
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <p key={workout._id}>{workout.title}</p>
+        ))}
+      </div>
     </div>
   )
 }
