cdfbf868436788d2d2dfcdd604d886b39ed66ddf
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-05 21:08:08 -0400

    add ContextProvider folder

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const WorkoutContext = createContext()
+
+export const WorkoutsContextProvider = () => {
+
+  return (
+    <WorkoutContext.Provider>
+      
+    </WorkoutContext.Provider>
+  )
+}
