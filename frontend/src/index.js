6d5c9a8f695b2a60875fc140a887128687824de1
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-08 12:58:21 -0400

    change proxy for deployement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutContext';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
