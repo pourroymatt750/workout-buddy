2f19c21fffe98ddb7f46f8411cac105478a59497
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-08 20:00:33 -0400

    delete extra comments

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -19,14 +19,6 @@ const WorkoutDetails = ({ workout, onUpdate }) => {
 
   const handleDeleteClick = async () => {
     setIsDeleteAlertVisible(true)
-    // const response = await fetch('/api/workouts/' + workout._id, {
-    //   method: 'DELETE'
-    // })
-    // const json = await response.json()
-
-    // if (response.ok) {
-    //   dispatch({type: 'DELETE_WORKOUT', payload: json})
-    // }
   }
 
   const handleUpdateClick = () => {
