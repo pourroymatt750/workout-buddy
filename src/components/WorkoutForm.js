03aa7e479ce0ea850ae166f3afc49f2ecf841892
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-09 16:46:30 -0400

    hardcode fetch paths

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -14,7 +14,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
 
-    const response = await fetch('/workouts', {
+    const response = await fetch('https://workout-buddy-backend-chyp.onrender.com/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
